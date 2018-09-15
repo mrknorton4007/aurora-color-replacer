47a99be61548d23c89363e86d4c1f446295d3388
mrknorton4007 <mrknorton4007@users.noreply.github.com>
2018-09-15 18:31:03 +0200

    use CWD + integrity folder check

--- aurora-color-replacer.py
+++ aurora-color-replacer.py
@@ -7,17 +7,31 @@ class AuroraColorReplacer:
 
     def __init__(self, theme_name):
         # current dir
-        self.base_path = os.path.dirname(os.path.abspath(__file__))
+        self.base_path = os.getcwd()
         # name of the folder containing the original files
         self.source_folder_name = "_input"
         # name of the folder that will contain the edited files
         self.output_folder_name = "_output"
         # import color replacing rules
         self.theme_rules = self._import_theme(theme_name)
+        # check folder structure
+        self._integrity_folder_check()
 
     def _import_theme(self, theme_name):
         # check and import py module with the replace rules
         pass
+
+    def _integrity_folder_check(self):
+        # ensure that input/output folders exists
+        source_folder = os.path.join(self.base_path, self.source_folder_name)
+        output_folder = os.path.join(self.base_path, self.output_folder_name)
+
+        for directory in [source_folder, output_folder]:
+            if not os.path.exists(directory):
+                # let the script raise an OSError and exit in case
+                # user doesnt have the correct permissions.
+                os.mkdir(directory)
+        return True
 
     def _get_replaced_line(self, xml_tag):
         # given a xml line, find a matching rule
