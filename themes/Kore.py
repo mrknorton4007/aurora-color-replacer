a9e2ba387601624a322c86a044bac6cbbc29c742
mrknorton4007 <mrknorton4007@users.noreply.github.com>
2019-03-26 17:17:01 +0100

    XBMC/Kodi theme test, Uplay as base

---
+++ themes/Kore.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# #####################################
+# Kore theme
+# Version: v0.2.0
+# Author: mrknorton4007
+# #####################################
+
+theme_rules = {
+    # FillColor
+    ('<FillColor>', '0xff444444'): '0xff121c1f',
+    ('<FillColor>', '0xff55b696'): '0xff0a5657', # dark aquamarine
+    ('<FillColor>', '0xffaaaaaa'): '0xff18252a',
+    ('<FillColor>', '0xffcccccc'): '0xff18252a',
+    ('<FillColor>', '0xffdddddd'): '0xff888888',
+    ('<FillColor>', '0xffebebeb'): '0xff999999',
+    ('<FillColor>', '0xffffffff'): '0xff18252a',
+    # Prop
+    ('<Prop>', '0xff33e5a8'): '0xff00cbc4', # aquamarine
+    ('<Prop>', '0xff444444'): '0xffffffff',
+    ('<Prop>', '0xff55b696'): '0xff0a5657', # dark aquamarine
+    # StrokeColor
+    ('<StrokeColor>', '0xff000000'): '0xffbbbbbb',
+    ('<StrokeColor>', '0xff0f0f80'): '0xffcccccc',
+    ('<StrokeColor>', '0xff444444'): '0xff121c1f',
+    # TextColor
+    ('<TextColor>', '0xff444444'): '0xffffffff',
+    ('<TextColor>', '0xffaaaaaa'): '0xffffffff',
+    ('<TextColor>', '0xffffffff'): '0xffffffff',
+}
