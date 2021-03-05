ad6d1cad3d603cd9f6e9fb3fc1650e335681a0b5
mrknorton4007 <mrknorton4007@users.noreply.github.com>
2021-03-05 21:43:11 +0100

    add NierAutomata theme (beta)

---
+++ themes/NierAutomata.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# #####################################
+# Nier:Automata theme
+# Version: v0.1.0
+# Author: mrknorton4007
+# #####################################
+
+theme_rules = {
+    # FillColor
+    # ('<FillColor>', '0xff000000'): '0xff000000',  # pure black
+    # ('<FillColor>', '0xff0000a7'): '0xff0000a7',  # unknown dark blue
+    # ('<FillColor>', '0xff0feb0f'): '0xff0feb0f',  # unknown neon green
+    # ('<FillColor>', '0xff33a5e8'): '0xff33a5e8',  # temperature graph blue
+    ('<FillColor>', '0xff33e5a8'): '0xff48473e',  # primary aquamarine
+    ('<FillColor>', '0xff444444'): '0xff48473e',
+    ('<FillColor>', '0xff55b696'): '0xffacaa97',  # secondary aquamarine
+    ('<FillColor>', '0xffaaaaaa'): '0xffacaa97',
+    # ('<FillColor>', '0xffbf3d3d'): '0xffbf3d3d',  # unknown red
+    # ('<FillColor>', '0xffcc9922'): '0xffcc9922',  # temperature graph orange
+    ('<FillColor>', '0xffcccccc'): '0xff48473e',
+    ('<FillColor>', '0xffdddddd'): '0xffc5c4ac',
+    # ('<FillColor>', '0xffeb0f0f'): '0xffeb0f0f',  # temperature graph red
+    ('<FillColor>', '0xffebebeb'): '0xffc5c4ac',
+    ('<FillColor>', '0xffffffff'): '0xffc5c4ac',
+    # Prop
+    # ('<Prop>', '0xff0000a7'): '0xff0000a7',
+    # ('<Prop>', '0xff0000cf'): '0xff0000cf',
+    # ('<Prop>', '0xff0900ef'): '0xff0900ef',
+    ('<Prop>', '0xff33e5a8'): '0xff48473e',  # primary aquamarine
+    ('<Prop>', '0xff444444'): '0xff48473e',
+    ('<Prop>', '0xff55b696'): '0xffacaa97',  # secondary aquamarine
+    # ('<Prop>', '0xff7b5f19'): '0xff7b5f19',
+    ('<Prop>', '0xffaaaaaa'): '0xffacaa97',
+    # ('<Prop>', '0xffb39444'): '0xffb39444',
+    # ('<Prop>', '0xffcc9922'): '0xffcc9922',
+    ('<Prop>', '0xffdddddd'): '0xffc5c4ac',
+    ('<Prop>', '0xffebebeb'): '0xffc5c4ac',
+    ('<Prop>', '0xffeeeeee'): '0xffc5c4ac',
+    ('<Prop>', '0xffffffff'): '0xffc5c4ac',
+    # StrokeColor
+    ('<StrokeColor>', '0xff000000'): '0xff48473e',
+    ('<StrokeColor>', '0xff0f0f80'): '0xff48473e',
+    ('<StrokeColor>', '0xff444444'): '0xff48473e',
+    # TextColor
+    ('<TextColor>', '0xff444444'): '0xff48473e',
+    ('<TextColor>', '0xffaaaaaa'): '0xff48473e',
+    # ('<TextColor>', '0xffebebeb'): '0xffc5c4ac',
+    ('<TextColor>', '0xffffffff'): '0xffc5c4ac'
+}
